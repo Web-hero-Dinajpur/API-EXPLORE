055c7d4111e48c57816cc252f87ffcba2279e0a2
Sreedhar-roy <sreedharroy0@gmail.com>
2024-10-02 14:08:36 +0600

    post json data commit done

--- post.js
+++ post.js
@@ -1,1 +1,21 @@
-console.log('hello api')
+
+function loadData(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(response => displayData(response))
+}
+function displayData(data){
+    const postContainer = document.getElementById('explor-api')
+    for(const user of data){
+        const postdiv = document.createElement('div')
+        postdiv.classList.add('post')
+        postdiv.innerHTML =`
+        <h4>User ${user.userId}</h4>
+        <h5>Title ${user.title}</h5>
+        <h4>User Description ${user.body}</h4>
+        `
+        postContainer.appendChild(postdiv)
+    }
+}
+
+loadData()
